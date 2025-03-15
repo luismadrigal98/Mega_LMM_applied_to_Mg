e6b9498249934fcf88d14092bb654f3af7c59bc0
Luis <madrigalrocalj@gmail.com>
2025-03-15 11:56:45 -0500

    Function for running the rrBLUP individually is now completely functional.

--- src/run_rrBLUP_baseline.R
+++ src/run_rrBLUP_baseline.R
@@ -1,12 +1,15 @@
-run_rrBLUP_baseline <- function(Y_train, sample_data, K, formula) 
+run_rrBLUP_baseline <- function(Y, sample_data, K, formula, 
+                                return_all_BLUP_values = FALSE) 
 {
   #' Run rrBLUP as a baseline comparison model
   #' 
-  #' @param Y_train Training phenotype matrix
+  #' @param Y Training phenotype matrix
   #' @param sample_data Data frame with individual information
   #' @param K Kinship/relationship matrix
   #' @param formula Model formula for fixed effects
-  #' @return Matrix of rrBLUP predictions
+  #' @param return_all_BLUP_values Logical indicating whether to return all BLUP 
+  #' values produced by mixed.solve.
+  #' @return Matrix of rrBLUP predictions or list containing predictions and BLUP values
   #' ___________________________________________________________________________
   
   # Extract fixed effect formula parts
@@ -16,26 +19,42 @@ run_rrBLUP_baseline <- function(Y_train, sample_data, K, formula)
   X <- model.matrix(fixed_formula, sample_data)
   
   # Initialize predictions matrix
-  rrBLUP_predictions <- matrix(NA, nrow(Y_train), ncol(Y_train), 
-                               dimnames = dimnames(Y_train))
+  rrBLUP_predictions <- matrix(NA, nrow(Y), ncol(Y), 
+                               dimnames = dimnames(Y))
+  
+  # Initialize container for BLUP results
+  BLUP_per_trait <- list()
   
   # Run rrBLUP separately for each predictor
-  for (i in 1:ncol(Y_train)) {
+  results <- foreach(i = 1:ncol(Y), .packages = "rrBLUP") %dopar% {
     # Check if we need to drop some fixed effects due to zero variance
     X_i <- X
     if (ncol(X) > 1) {
-      var_check <- apply(X[!is.na(Y_train[, i]), , drop = FALSE], 2, var)
+      var_check <- apply(X[!is.na(Y[, i]), , drop = FALSE], 2, var)
       if (any(var_check == 0)) {
         X_i <- X[, var_check > 0, drop = FALSE]
       }
     }
     
     # Fit model
-    res <- mixed.solve(y = Y_train[, i], X = X_i, K = K)
+    res <- mixed.solve(y = Y[, i], X = X_i, K = K)
     
     # Make predictions
-    rrBLUP_predictions[, i] <- c(X_i %*% res$beta) + res$u
+    predictions <- c(X_i %*% res$beta) + res$u
+    
+    # Return both predictions and model results
+    list(predictions = predictions, model_fit = res)
   }
   
-  return(rrBLUP_predictions)
+  # Extract results from foreach
+  for (i in 1:ncol(Y)) {
+    rrBLUP_predictions[, i] <- results[[i]]$predictions
+    BLUP_per_trait[[colnames(Y)[i]]] <- results[[i]]$model_fit
+  }
+  
+  if (return_all_BLUP_values) {
+    return(list(predictions = rrBLUP_predictions, 
+                BLUP_values = BLUP_per_trait))
+  }
+  else return(rrBLUP_predictions)
 }
